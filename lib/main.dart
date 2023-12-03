7df22f2510e08e14661045baf7f8139a09c01f41
Davey Schulz <davey.schulz@hotmail.com>
2023-12-03 11:05:40 +0100

    Setup router

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:routemaster/routemaster.dart';
 import 'package:share_app/features/auth/screens/login_screen.dart';
 import 'package:share_app/firebase_options.dart';
+import 'package:share_app/router.dart';
 import 'package:share_app/theme/palette.dart';
 
 void main() async {
@@ -22,11 +24,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Share App',
       theme: Palette.darkModeAppTheme,
-      home: const LoginScreen(),
+      routerDelegate: RoutemasterDelegate(
+        routesBuilder: (context) => loggedOutRoute,
+      ),
+      routeInformationParser: const RoutemasterParser(),
     );
   }
 }
